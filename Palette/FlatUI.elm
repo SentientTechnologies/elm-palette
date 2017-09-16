15e1302bb31027b25e431fa33b37647636a85c1f
Aaron Dai <aaron.dai@sentient.ai>
2017-09-15 22:22:52 -0700

    add FlatUI color themes

---
+++ Palette/FlatUI.elm
@@ -0,0 +1,149 @@
+module Palette.FlatUI exposing (..)
+
+{-| FlatUI Palette
+
+@docs turquoise, emerald, peterRiver, amethyst, wetAsphalt, greenSea, nephritis, belizeHole, wisteria, midnightBlue, sunFlower, carrot, alizarin, clouds, concrete, orange, pumpkin, pomegranate, silver, asbestos
+
+-}
+
+import Color exposing (Color, rgb)
+
+
+{-| turquoise
+-}
+turquoise : Color
+turquoise =
+    rgb 26 188 156
+
+
+{-| emerald
+-}
+emerald : Color
+emerald =
+    rgb 46 204 113
+
+
+{-| peterRiver
+-}
+peterRiver : Color
+peterRiver =
+    rgb 52 152 219
+
+
+{-| amethyst
+-}
+amethyst : Color
+amethyst =
+    rgb 155 89 182
+
+
+{-| wetAsphalt
+-}
+wetAsphalt : Color
+wetAsphalt =
+    rgb 52 73 94
+
+
+{-| greenSea
+-}
+greenSea : Color
+greenSea =
+    rgb 22 160 133
+
+
+{-| nephritis
+-}
+nephritis : Color
+nephritis =
+    rgb 39 174 96
+
+
+{-| belizeHole
+-}
+belizeHole : Color
+belizeHole =
+    rgb 41 128 185
+
+
+{-| wisteria
+-}
+wisteria : Color
+wisteria =
+    rgb 142 68 173
+
+
+{-| midnightBlue
+-}
+midnightBlue : Color
+midnightBlue =
+    rgb 44 62 80
+
+
+{-| sunFlower
+-}
+sunFlower : Color
+sunFlower =
+    rgb 241 196 15
+
+
+{-| carrot
+-}
+carrot : Color
+carrot =
+    rgb 230 126 34
+
+
+{-| alizarin
+-}
+alizarin : Color
+alizarin =
+    rgb 231 76 60
+
+
+{-| clouds
+-}
+clouds : Color
+clouds =
+    rgb 236 240 241
+
+
+{-| concrete
+-}
+concrete : Color
+concrete =
+    rgb 149 165 166
+
+
+{-| orange
+-}
+orange : Color
+orange =
+    rgb 243 156 18
+
+
+{-| pumpkin
+-}
+pumpkin : Color
+pumpkin =
+    rgb 211 84 0
+
+
+{-| pomegranate
+-}
+pomegranate : Color
+pomegranate =
+    rgb 192 57 43
+
+
+{-| silver
+-}
+silver : Color
+silver =
+    rgb 189 195 199
+
+
+{-| asbestos
+-}
+asbestos : Color
+asbestos =
+    rgb 127 140 141
